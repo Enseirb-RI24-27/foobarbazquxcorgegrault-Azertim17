bfefec8c7aa158585ca15c7b3d653410c4a27c3d
Tim <mazzolinitim@gmail.com>
2026-01-12 17:02:29 +0100

    Add Getters

--- src/main/java/com/jad/Foo.java
+++ src/main/java/com/jad/Foo.java
@@ -1,14 +1,17 @@
 package com.jad;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Foo {
+
     private Bar bar;
     private List<Baz> bazs;
     private Qux qux;
 
     public Foo(Bar bar) {
         this.bar = bar;
+        this.bazs = new ArrayList<>();
         this.qux = new Qux();
     }
 
@@ -16,9 +19,20 @@ public class Foo {
         this.bazs.add(baz);
     }
 
-
     public void addGrault(){
 
+    }
+
+    public Bar getBar() {
+        return this.bar;
+    }
+
+    public List<Baz> getBazs() {
+        return this.bazs;
+    }
+
+    public Qux getQux() {
+        return this.qux;
     }
 
 }
