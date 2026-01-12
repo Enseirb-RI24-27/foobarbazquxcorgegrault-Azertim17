e71b0fb72fafc488918fdc368b505fcfd8d4ec5c
Tim <mazzolinitim@gmail.com>
2026-01-12 16:55:15 +0100

    Add Qux

--- src/main/java/com/jad/Foo.java
+++ src/main/java/com/jad/Foo.java
@@ -5,9 +5,11 @@ import java.util.List;
 public class Foo {
     private Bar bar;
     private List<Baz> bazs;
+    private Qux qux;
 
     public Foo(Bar bar) {
         this.bar = bar;
+        this.qux = new Qux();
     }
 
     public void addBaz(Baz baz){
